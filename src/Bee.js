ac98c4bb7de7e9bce87ae698551472cbc8addafd
Matthew Pak <mattpak@gmail.com>
2022-02-28 11:31:17 -0800

    Passed Grub and Bee tests

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,14 @@
 var Bee = function() {
+  // delegate to grub superclass
+  Grub.call(this, null);
+  // reset age/color properties
+  this.age = 5;
+  this.color = 'yellow';
+  // new job property
+  this.job = 'keep on growing';
 };
 
+// set bee prototype to delegate to grub's
+Bee.prototype = Object.create(Grub.prototype);
+// set bee constructor to bee's prototype
+Bee.prototype.constructor = Bee;
--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,12 @@
 var Grub = function() {
+  // set age, color, food properties
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
+};
+
+// add eat method
+Grub.prototype.eat = function() {
+
 };
 
