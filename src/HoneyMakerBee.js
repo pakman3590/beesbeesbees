8718114eb4988959d46ecb7cf87605cbfd56abef
Matthew Pak <mattpak@gmail.com>
2022-02-28 11:35:51 -0800

    Passed HoneyMakerBee tests

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,22 @@
 var HoneyMakerBee = function() {
+  // call bee superclass
+  Bee.call(this);
+  // reassign age and job properties
+  this.age = 10;
+  this.job = 'make honey';
+  // new honeypot property
+  this.honeyPot = 0;
 };
 
+// delegate honeybee prototype to bee
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+// set honey bee constructor
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+// new makehoney method
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot ++;
+};
+//new givehoney method
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot --;
+};
