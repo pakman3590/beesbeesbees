6c9ee31938d10a0a0d5d67adf44683c1c977dc23
Matthew Pak <mattpak@gmail.com>
2022-02-28 11:46:04 -0800

    Passed RetiredForagerBee tests

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,22 @@
 var RetiredForagerBee = function() {
+  // call foragerbee superclass
+  ForagerBee.call(this);
+  // reassign age, job, canfly, color properties
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+// delegate retired prototype to forager prototype
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+// set retired constructor
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+// new gamble method
+RetiredForagerBee.prototype.gamble = function (treasure) {
+  this.treasureChest.push(treasure);
+},
+// update forage method
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+};
