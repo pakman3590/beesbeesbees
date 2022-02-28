a2eb1b311cdf3a50b0cd86da9ebb6e186bbb445d
Matthew Pak <mattpak@gmail.com>
2022-02-28 11:40:12 -0800

    Passed ForagerBee tests

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,19 @@
 var ForagerBee = function() {
+  // call bee superclass
+  Bee.call(this);
+  // reassign age, job properties
+  this.age = 10;
+  this.job = 'find pollen';
+  // new canfly and treasurechest properties
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+// delegate foragerbee prototype to bee prototype
+ForagerBee.prototype = Object.create(Bee.prototype);
+// set forager constructor
+ForagerBee.prototype.constructor = ForagerBee;
+// new forage method
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+};
